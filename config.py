9ef8e8d7a2f8d595435c7f884d0ee25ddb8967e1
CHALCOGEN <166417113+CHALCOGEN645@users.noreply.github.com>
2024-05-19 13:18:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,10 +27,10 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="7025035259").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="7004770944").split()))
 for x in OXYGEN:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="7025035259"))
+OWNER_ID = int(getenv("OWNER_ID", default="7004770944"))
 SUDO_USERS.append(OWNER_ID)
 
 
