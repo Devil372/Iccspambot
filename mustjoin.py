7b39831577c00472ce67540863e9658a210c2da2
CHALCOGEN <166417113+CHALCOGEN645@users.noreply.github.com>
2024-05-19 14:30:20 +0530

    Update mustjoin.py

--- mustjoin.py
+++ mustjoin.py
@@ -7,9 +7,7 @@ from PENGUINMUSIC import app
 #--------------------------
 
 MUST_JOIN = "PENGUIN_X_SUPPORT"
-MUST_JOIN = "PENGUIN_X_CHAT"
-MUST_JOIN = "@falcon_security"
-MUST_JOIN = "@indian_hacker_group"
+MUST_JOIN = "indiancybercriminal"
 
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
